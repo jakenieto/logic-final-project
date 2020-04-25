34d54b46cf6d7865c52a0237d045b8309f514ece
Victor Kalev <vkalev@victors-macbook.lan>
2020-04-24 23:19:41 -0500

    multi car comments

--- rush-hour-mult-cars.rkt
+++ rush-hour-mult-cars.rkt
@@ -1,10 +1,18 @@
 #lang forge
 
-//4X4 board
+/**
+  RUSH HOUR (MULTIPLE CARS)
+
+  Implementation of the slide block logic puzzle game Rush Hour, in which you slide
+  the blocking vehicles out of the way for the red car to exit. Instead of solely
+  focusing on only two cars, this file is generalized so that more than one other
+  blocking car is considered.
+*/
+
+//---------SIG DEFINITIONS---------//
 
 /*
-  This sig models an abstract square on the
-  rush hour board.
+  This sig models an abstract square on the rush hour board.
 */
 abstract sig Square {
     left: lone Square,
@@ -13,20 +21,25 @@ abstract sig Square {
     down: lone Square
 }
 
+/*
+  Identification of target car and blocking cars.
+*/
 abstract sig Color {}
-
 one sig Red extends Color {}
 one sig Other extends Color {}
 
-sig Car{
+/*
+  This sig models a car in the game, which contains a color (to be able to specify
+  the target car) and its orientation (specifying which direction it can move in).
+*/
+sig Car {
     color: one Color,
     ori: one Orientation
 }
 
 
 /*
-  Actual instances of squares. There are
-  16 for a 4X4 board.
+  Actual instances of squares. There are 16 for a 4X4 board.
 */
 one sig Square00 extends Square {}
 one sig Square01 extends Square {}
@@ -46,11 +59,9 @@ one sig Square32 extends Square {}
 one sig Square33 extends Square {}
 
 /*
-  Models the orientation that a car has
-  relative to the board. This direction
-  describes the possible direction of motion
-  that the car can take. The two direction
-  types are "horizontal" and "vertical".
+  Models the orientation that a car has relative to the board. This direction
+  describes the possible direction of motion that the car can take. The two
+  direction types are "horizontal" and "vertical".
 */
 abstract sig Orientation {}
 one sig Vertical extends Orientation {}
@@ -58,43 +69,50 @@ one sig Horizontal extends Orientation {}
 
 
 /*
-  This models a state in the gameplay 
+  This models a state in the gameplay. Each state keep track of the squares
+  that each car is currently in, as well as a variable to store the next car
+  to be moved (ensuring that the cars are moved one at a time).
 */
 sig State {
     carLoc: set Car->Square,
     toMove: one Car
-   
-   
-
 }
 
 
-/*
-  Initial state of the game. Must set up
-  each squares neighboring squares.
-*/
+//---------STATE DEFINITIONS---------//
 
+
+/*
+  Initial state of the game. Must set up each squares neighboring squares,
+  define the sizes that each car can take on, define the red car, the squares
+  that each car exists within (and that they are valid), and one car that is
+  is set to move next.
+*/
 state[State] initState {
     validCarSizes[carLoc]
     hasRedCar[carLoc]
     some carLoc
     validLoc[carLoc]
     one toMove
- 
-
 }
 
+/*
+  Final state of the game. Ensures that a red car must win (reach the end tile)
+  in the final state, defines one car must still move at a time and each car's
+  location must be valid.
+*/
 state[State] finalState {
     redCarWins[carLoc]
     one toMove
     validLoc[carLoc]
-   
-
 }
 
 
+//------------GAME RULES------------//
+
+
+//TODO
 transition[State] puzzle {
-    
     all car: Car {
         #(car.carLoc) = #(car.carLoc')
         car = toMove implies {
@@ -103,14 +121,13 @@ transition[State] puzzle {
         car != toMove implies {
              car.carLoc' = car.carLoc
         }
-
     }
     one toMove'
 }
 
-
 /*
-  Can we do separate files and include imports?
+  Defines all of the squares of the board and their left, right, up and down
+  squares.
 */
 pred setupBoard {
     no Square00.left
@@ -192,21 +209,51 @@ pred setupBoard {
     Square33.up = Square23
     no Square33.down 
     no Square33.right 
-
 }
 
 /*
-  Pred to setup the initial car's locations.
+  Ensures that for each state, none of the cars overlap -- none of the squares
+  of a car exists in the carLoc squares of another car. 
 */
+pred noOverlap {
+   all s: State {
+       all c1,c2: Car {
+           c1 != c2 implies {
+               all s1: c1.(s.carLoc) | s1 not in c2.(s.carLoc)
+               all s2: c2.(s.carLoc) | s2 not in c1.(s.carLoc)
+           } 
+       }
+   }
+}
+
+/*
+  General set of game rules that ensures there is only one red car, and calls
+  two of the other game-defining predicates -- noOverlap and setupBoard.
+*/
+pred gameRules{
+   #(color.Red) = 1
+   noOverlap
+   setupBoard
+}
 
 
+//---------TRANSITION PREDICATES---------//
+
+
+/*
+  Defines each car to be at least of length 2.
+*/
 pred validCarSizes[carLoc: set Car->Square] {
     all c: Car | #(c.carLoc) >= 2
 
 }
 
+/*
+  Ensures that there exists a red car (target car) that is colored red (which enables
+  us to identify it during each transition), has horizontal orientation, is of size 2,
+  and does begin on the end squares.
+*/
 pred hasRedCar[carLoc: set Car->Squares] {
-  
     one c: Car {
         c.color = Red
         c.ori = Horizontal
@@ -214,82 +261,64 @@ pred hasRedCar[carLoc: set Car->Squares] {
         END not in c.carLoc
         Square12 not in c.carLoc
     }
-        
-    
-
 }
 
+/*
+  Ensures that in the end state the red car exists on the two end squares.
+*/
 pred redCarWins[carLoc: set Car->Square] {
     all c: Car {
          c.color = Red implies {
             c.carLoc = END + Square12
         }
     }
-
-
 }
 
-pred noOverlap {
-   all s: State {
-       all c1,c2: Car {
-           c1 != c2 implies {
-               all s1: c1.(s.carLoc) | s1 not in c2.(s.carLoc)
-               all s2: c2.(s.carLoc) | s2 not in c1.(s.carLoc)
-           }
-           
-       }
-   }
-}
-
-
-
-pred gameRules{
-   #(color.Red) = 1
-   //#(carLoc) > 11
-  // #(Car) = 4
-   //all s: State | one c: Car | #(c.(s.carLoc)) > 3
-   noOverlap
-   setupBoard
-
-}
-
+/*
+  Ensures that each car's current position is valid.
+*/
 pred validLoc[loc: set Car->Square] {
     all c: Car {
         all s: c.loc {
+            // Two cases depending on the current car's orientation.
             c.ori = Horizontal implies {
+                // Ensures that the car continues to remain connected.
                 some s1:  (c.loc - s) | s1 = s.left or s1 = s.right
+                // Allows for cars to move more than one square at a time.
                 all s2: (c.loc - s) | s2 in s.^(left + right)
+                // Disallows vertical squares from being accessed by a horizontal car.
                 no s3: (c.loc - s) | (s.up = s3 or s.down = s3)
             }
             c.ori = Vertical implies {
+                // Ensures that the car continues to remain connected.
                 some s1:  (c.loc - s) | s1 = s.up or s1 = s.down
+                // Allows for cars to move more than one square at a time.
                 all s2: (c.loc - s) | s2 in s.^(up + down)
+                // Disallows horizontal squares from being accessed by a vertical car.
                 no s3: (c.loc - s) | (s.left = s3 or s.right = s3)
             }
         }
     }
 }
-//carLoc,carLoc',carOri,car
 
+/*
+  Ensures that a car's position in the post state is valid.
+*/
 pred validMove[startLoc: set Car->Square, endLoc: set Car->Square,car: one Car] {
- 
-   car.ori = Horizontal implies{
+    // Two cases based on the car's orientation.
+   car.ori = Horizontal implies {
         all s: car.startLoc {
-            //Possible transitive closure like operation to do??
+            // Horizontal cars can only move into left or right squares on the same row.
             all bs: (Square - (s.^(left + right))) | bs not in car.endLoc
-            //all bs: (Square - (s.^(left + right) s.right + s + s.left.left + s.right.right + s.left.right + s.right.left)) | bs not in endLoc
         }
-
     }
     car.ori = Vertical implies {
         all s: car.startLoc {
+            // Vertical cars can only move into up or down squares in the same column.
             all bs: (Square - (s.^(up + down)))| bs not in car.endLoc
         }
-    }
-    
+    }   
     validLoc[endLoc]
-    
-
 }
 
 pred optimal {
@@ -297,34 +326,57 @@ pred optimal {
    all s1,s2: State | s1 != s2 implies {s1.carLoc != s2.carLoc}
 }
 
+
+//------------GENERAL CHECKS------------//
+
+
+/*
+  Checks that a car doesn't have squares that exist diagonally / disconnected from its other ones.
+*/
 pred noDiagonal[loc: set Car->Square] {
       all c: Car {
         all sq: c.loc {
+            // Two cases based on the car's orientation.
             c.ori = Horizontal implies {
+                // Makes sure no squares exist outside of the row of the current horizontal car.
                 no s: (Square - (c.loc).^(left + right)) | s in c.loc
             }
             c.ori = Vertical implies {
+                 // Makes sure no squares exist outside of the column of the current vertical car.
                 no s: (Square - (c.loc).^(up + down)) | s in c.loc
             }
         }
     }
 }
 
-pred welformedCars {
+/*
+  General statement to verify that the algorithm is behaving as it should be (no cars become ill formed).
+*/
+pred wellFormedCars {
     all s: State {
         noDiagonal[s.carLoc]
     }
- 
 }
             
-
+/*
+check<|traces|> {(gameRules and optimal) => wellFormedCars} for  2 State
+check<|traces|> {(gameRules and optimal) => wellFormedCars} for  3 State
+check<|traces|> {(gameRules and optimal) => wellFormedCars} for  4 State
+check<|traces|> {(gameRules and optimal) => wellFormedCars} for  5 State
+check<|traces|> {(gameRules and optimal) => wellFormedCars} for  6 State
+check<|traces|> {(gameRules and optimal) => wellFormedCars} for  7 State
+check<|traces|> {(gameRules and optimal) => wellFormedCars} for  8 State
+check<|traces|> {(gameRules and optimal) => wellFormedCars} for  9 State
+check<|traces|> {(gameRules and optimal) => wellFormedCars} for  10 State
+*/
 
 --trace<|State, initState, puzzle, finalState|> traces: linear {}
-
 --run<|traces|> {gameRules optimal} for  8 State
 
 
-//testing
+//---------------TESTING---------------//
+
+
 /*
 inst discontinuous {
     State = State0 + State1 + State2 + State3 + State4 + State5 + State6 + State7 
@@ -360,17 +412,7 @@ test expect {
 */
 trace<|State, initState, puzzle, finalState|> traces: linear {}
 
-/*
-check<|traces|> {(gameRules and optimal) => welformedCars} for  2 State
-check<|traces|> {(gameRules and optimal) => welformedCars} for  3 State
-check<|traces|> {(gameRules and optimal) => welformedCars} for  4 State
-check<|traces|> {(gameRules and optimal) => welformedCars} for  5 State
-check<|traces|> {(gameRules and optimal) => welformedCars} for  6 State
-check<|traces|> {(gameRules and optimal) => welformedCars} for  7 State
-check<|traces|> {(gameRules and optimal) => welformedCars} for  8 State
-check<|traces|> {(gameRules and optimal) => welformedCars} for  9 State
-check<|traces|> {(gameRules and optimal) => welformedCars} for  10 State
-*/
+
 
 
 
